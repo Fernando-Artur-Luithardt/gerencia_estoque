57119c1a107d7401d70dc3a4bc4ed372454737d8
Fernando Artur Luithardt <fluithardt@gmail.com>
2022-03-22 16:09:44 -0300

    Nav Bar

---
+++ inc_menu.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">i4i</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+<div class="collapse navbar-collapse" id="navbarsExample02">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Administração Materiais</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="addCond.php"></a>
+          </li>
+        </ul>
+        <!-- <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search">
+        </form> -->
+        <ul class="navbar-nav mr-auto">
+          <li></li>
+          <li class="nav-item active">
+            <a class="nav-link" href="funcoes/logout.php">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
