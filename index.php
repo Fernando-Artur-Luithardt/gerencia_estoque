8bcf419bda6bcc558250c4a0cdb820c2dcddcc8e
Fernando Artur Luithardt <fluithardt@gmail.com>
2022-03-22 00:07:43 -0300

    index.php (login) e css básico(style.css)

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>i4i</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+<body>
+    <form action="funcoes/login.php" method="post" id="formLogin">
+        <div id="container">
+            <div>
+                <h1>login</h1>
+                <br>
+                <h3>administração</h3>
+                <br>
+            </div class="input">
+            <div>
+                <input type="text" name="id" required>
+            </div>
+
+            <div class="input">
+                <input type="password" name="senha" required>
+            </div>
+
+            <div class="input">
+                <input type="submit">
+            </div>
+        </div>
+    </form>
+<script>
+    
+</body>
+</html>
