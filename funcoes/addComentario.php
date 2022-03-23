a2a5b6147d3d8928e02b4def6562c7e56c567d99
Fernando Artur Luithardt <fluithardt@gmail.com>
2022-03-22 21:44:06 -0300

    correção: $_Request codMaterial, header e id usuário

--- funcoes/addComentario.php
+++ funcoes/addComentario.php
@@ -3,15 +3,15 @@
     require 'banco.php';
 
     $comentario = $_REQUEST["comentario"];
+    $codMaterial = $_REQUEST['id'];
     $userId = $_SESSION['id'];
-    $codMaterial = isset($_POST['material'])? $_POST['material']: "";
-    echo $codMaterial;
+
     if(empty($comentario) || empty($userId)) {
         $response = array('mensagem' => "campos obrigatórios faltando");
         exit;
     }
 
-    $sql = "INSERT INTO `comentario` (`comentario`,`user_id`,`material`) VALUES ('$comentario','$userId','$codMaterial')";
+    $sql = "INSERT INTO `comentario` (`comentario`,`user_id`,`material`) VALUES ('$comentario','$userId[id]','$codMaterial')";
     $cadastro = mysqli_query($conn, $sql);
 
     if (!$cadastro) {
@@ -21,6 +21,6 @@
         echo $responseJson;
         exit;
     }else {
-        // header("Location: http://localhost/i4i/comentarios.php");
+        header("Location: http://localhost/i4i/comentarios.php?id=$codMaterial");
     }
 ?>  
